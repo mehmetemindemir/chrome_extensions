43e9a24985a627af097d6c112213064f78b12b87
med <eminmehmet.demir@gamil.com>
2020-08-12 15:37:04 -0400

    first commit

--- content.js
+++ content.js
@@ -12,17 +12,16 @@ let answersList=[];
 let count=0;
 let  corr=null;
 let question ={
-  "question":null,
-  "text":"",
-  "img":[],
-  "qid":""
+  "QuestionDetail":null,
+  "QuestionBody":"",
+  "OriginalId":""
 };
 let questionBody ;
 let quest = null;
 let answer;
 let finish=false;
 const qLists = Array.from(document.querySelectorAll('.qsb-div'));
-const chooses=Array.from(document.querySelectorAll('.ans-let'));
+let chooses=Array.from(document.querySelectorAll('.ans-let'));
 let answerNum=Math.floor(Math.random() * chooses.length)+1;  
 answerNum=1;
 
@@ -46,14 +45,23 @@ if(document.getElementById("ctl00_CPH_Main_TestSettings_LB_StartTest")){
 
 if(qLists.length>0 && count<qLists.length && answersList.length<1){
   question={
-    "text":document.getElementById("Div_qs").innerText,
-    "imgs":[],
-    "qid":document.getElementById("L_QsID").innerText.replace("Nº ",""),
-    "explanation":document.getElementById("TabPanel_Exp").innerHTML,
-    "subareaid":getCookie("subareaid"),
+    "QuestionBody":document.getElementById("Div_qs").innerText,
+    "ImageList":[],
+    "OriginalId":document.getElementById("L_QsID").innerText.replace("Nº ",""),
+    "Explanation":document.getElementById("TabPanel_Exp").innerHTML,
+    "QuestionClassRoot":getCookie("subareaid"),
+  }
+  questionBody={  "shuffleanswers": "1",   "single": "true",  "answernumbering": "abc", "answer":[]  };
+                  
+  c1=document.getElementById("tr_answer1").children[1].innerHTML;
+  c2=document.getElementById("tr_answer2").children[1].innerHTML;
+  c3=document.getElementById("tr_answer3").children[1].innerHTML;
+  if(chooses.length==4){
+    c4=document.getElementById("tr_answer4").children[1].innerHTML;
+    
   }
   for (let index = 0; index <  Array.from(document.querySelectorAll('#Panel_Figures img')).length; index++) {
-    question.imgs.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
+    question.ImageList.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
     //console.log( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src)  ;                 
   }
   setTimeout( () => { document.getElementById("tr_answer"+answerNum).click(); }, 2000);  
@@ -68,44 +76,44 @@ function runRequestScript(message) {
         if (qLists.length>0 && count+1>qLists.length && !finish) {
           setTimeout( () => { 
               question={
-                "text":document.getElementById("Div_qs").innerText,
-                "imgs":[],
-                "qid":document.getElementById("L_QsID").innerText.replace("Nº ",""),
-                "explanation":document.getElementById("TabPanel_Exp").innerHTML,
-                "subareaid":getCookie("subareaid"),
+                "QuestionBody":document.getElementById("Div_qs").innerText,
+                "ImageList":[],
+                "OriginalId":document.getElementById("L_QsID").innerText.replace("Nº ",""),
+                "Explanation":document.getElementById("TabPanel_Exp").innerHTML,
+                "QuestionClassRoot":getCookie("subareaid"),
               }
-                questionBody={  "shuffleanswers": "1",   "single": "true",  "answernumbering": "abc","_type": "multiplechoice", "answer":[] ,"img":[] };
+                questionBody={  "shuffleanswers": "1",   "single": "true",  "answernumbering": "abc", "answer":[]  };
                 c1=document.getElementById("tr_answer1").children[1].innerHTML;
                 c2=document.getElementById("tr_answer2").children[1].innerHTML;
                 c3=document.getElementById("tr_answer3").children[1].innerHTML;
-                answer={ "text":c1,"_fraction":0};
+                answer={ "text":c1,"fraction":0};
                 if(document.getElementById("tr_answer1").className.indexOf("tr-ans-corr") !== -1){
-                  answer._fraction=100;
+                  answer.fraction=100;
                 }
                 questionBody.answer.push(answer);
-                answer={ "text":c2,"_fraction":0};
+                answer={ "text":c2,"fraction":0};
                 if(document.getElementById("tr_answer2").className.indexOf("tr-ans-corr") !== -1){
-                  answer._fraction=100;
+                  answer.fraction=100;
                 }
                 questionBody.answer.push(answer);
-                answer={ "text":c3,"_fraction":0};
+                answer={ "text":c3,"fraction":0};
                 if(document.getElementById("tr_answer3").className.indexOf("tr-ans-corr") !== -1){
-                  answer._fraction=100;
+                  answer.fraction=100;
                 }
                 questionBody.answer.push(answer);
                 if(chooses.length==4){
                   c4=document.getElementById("tr_answer4").children[1].innerHTML;
-                  answer={ "text":c4,"_fraction":0};
+                  answer={ "text":c4,"fraction":0};
                   if(document.getElementById("tr_answer4").className.indexOf("tr-ans-corr") !== -1){
-                    answer._fraction=100;
+                    answer.fraction=100;
                   }
                   questionBody.answer.push(answer);
                 }
                 
 
-                question.question=questionBody;
+                question.QuestionDetail=questionBody;
                 for (let index = 0; index <  Array.from(document.querySelectorAll('#Panel_Figures img')).length; index++) {
-                  question.imgs.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
+                  question.ImageList.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
                   console.log( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src)   ;                 
                 }
                 questions.push(question);
@@ -113,7 +121,8 @@ function runRequestScript(message) {
                 questions.forEach(element => {
 
                   var xhr = new XMLHttpRequest();
-                  var url = "https://ws.mehmetemindemir.com/questions";
+                  //var url = "https://ws.mehmetemindemir.com/questions";
+                  var url = "https://api.cbtroom.com/api/Question";
                   xhr.open("POST", url, true);
                   xhr.setRequestHeader("Content-Type", "application/json");
                   xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
@@ -125,6 +134,7 @@ function runRequestScript(message) {
                     }
                   }
                   var data = JSON.stringify(element);
+                   console.log("questions :"+JSON.stringify(element));
                   xhr.send(data);
                 });
               console.log(questions.length);
@@ -144,72 +154,79 @@ function runRequestScript(message) {
         }
         if (qLists.length>0 && count+1<qLists.length) {
 
-            if(clickA){               
-                      console.log("get answer and next question ")
+            if(clickA){              
+              clickA=!clickA;   
+                console.log("get answer and next question ")
                 setTimeout( () => { 
-                  questionBody={  "shuffleanswers": "1",   "single": "true",  "answernumbering": "abc","_type": "multiplechoice", "answer":[],"img":[]  };
-                  
-                  c1=document.getElementById("tr_answer1").children[1].innerHTML;
-                  c2=document.getElementById("tr_answer2").children[1].innerHTML;
-                  c3=document.getElementById("tr_answer3").children[1].innerHTML;
+                 
                   try {
-                    answer={ "text":c1,"_fraction":0};
+                    answer={ "text":c1,"fraction":0};
                   if(document.getElementById("tr_answer1").className.indexOf("tr-ans-corr") !== -1){
-                    answer._fraction=100;
+                    answer.fraction=100;
                   }
                   questionBody.answer.push(answer);
-                  answer={ "text":c2,"_fraction":0};
+                  answer={ "text":c2,"fraction":0};
                   if(document.getElementById("tr_answer2").className.indexOf("tr-ans-corr") !== -1){
-                    answer._fraction=100;
+                    answer.fraction=100;
                   }
                   questionBody.answer.push(answer);
-                  answer={ "text":c3,"_fraction":0};
+                  answer={ "text":c3,"fraction":0};
                   if(document.getElementById("tr_answer3").className.indexOf("tr-ans-corr") !== -1){
-                    answer._fraction=100;
+                    answer.fraction=100;
                   }
                   questionBody.answer.push(answer);
-                  if(chooses.length==4){
-                    c4=document.getElementById("tr_answer4").children[1].innerHTML;
-                    answer={ "text":c4,"_fraction":0};
-                    if(document.getElementById("tr_answer4").className.indexOf("tr-ans-corr") !== -1){
-                      answer._fraction=100;
+                    if(chooses.length==4){
+                      answer={ "text":c4,"fraction":0};
+                      if(document.getElementById("tr_answer4").className.indexOf("tr-ans-corr") !== -1){
+                        answer.fraction=100;
+                      }
+                      questionBody.answer.push(answer);
                     }
-                    questionBody.answer.push(answer);
-                  }
                   } catch (error) {
                     
                   }
                   
                   
                     for (let index = 0; index <  Array.from(document.querySelectorAll('#Panel_Figures img')).length; index++) {
-                      question.imgs.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
+                      question.ImageList.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
                       console.log( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src)   ;                 
                     }
-                  question.question=questionBody;
+                  question.QuestionDetail=questionBody;
                   questions.push(question);
                   
                   count++; 
                   qLists[count].children[0].click();
-                  clickA=!clickA;   
+                   
                 }, 5000);     
               
           } else if(!clickA){
+            clickA=!clickA;
               setTimeout( () => { 
+                chooses=Array.from(document.querySelectorAll('.ans-let'));
                 console.log("get question and next answer ")
                 question={
-                  "text":document.getElementById("Div_qs").innerText,
-                  "imgs":[],
-                  "qid":document.getElementById("L_QsID").innerText.replace("Nº ",""),
-                  "explanation":document.getElementById("TabPanel_Exp").innerHTML,
-                  "subareaid":getCookie("subareaid"),
+                  "QuestionBody":document.getElementById("Div_qs").innerText,
+                  "ImageList":[],
+                  "OriginalId":document.getElementById("L_QsID").innerText.replace("Nº ",""),
+                  "Explanation":document.getElementById("TabPanel_Exp").innerHTML,
+                  "QuestionClassRoot":getCookie("subareaid"),
                 }
+                questionBody={  "shuffleanswers": "1",   "single": "true",  "answernumbering": "abc", "answer":[]  };
+                  
+                  c1=document.getElementById("tr_answer1").children[1].innerHTML;
+                  c2=document.getElementById("tr_answer2").children[1].innerHTML;
+                  c3=document.getElementById("tr_answer3").children[1].innerHTML;
+
+                  if(chooses.length==4){
+                    c4=document.getElementById("tr_answer4").children[1].innerHTML;                   
+                  }
                 for (let index = 0; index <  Array.from(document.querySelectorAll('#Panel_Figures img')).length; index++) {
-                  question.imgs.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
+                  question.ImageList.push( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);
                   console.log( Array.from(document.querySelectorAll('#Panel_Figures img'))[index].src);                 
                 }
               
                 document.getElementById("tr_answer"+answerNum).click();
-                clickA=!clickA;
+                
             }, 1000);   
            
           }
